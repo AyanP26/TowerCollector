c1c8149f59679f60e3b4c06b27000b90dc57a285
Ikshaan Kaul <amazingdude1940@outlook.com>
2026-05-23 18:31:57 -0400

    got follow path working

--- tower-collector/Scripts/enemy.gd
+++ tower-collector/Scripts/enemy.gd
@@ -1,17 +1,17 @@
 extends RigidBody2D
+@export var follow_path: PathFollow2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var enemy_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = enemy_types.pick_random()
 	$AnimatedSprite2D.play()
-	
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	global_position = 
-
-
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free()
+func _physics_process(delta: float) -> void:
+	if follow_path == null:
+		print("fn")
+		return
+	
+	follow_path.progress += 20 * delta
+	global_position = follow_path.global_position

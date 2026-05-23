932f5dec4c7fe805806784a7097c4da0fb78f080
codeall1234 <1017061@lcps.org>
2026-05-23 13:54:19 -0400

    merged?

--- tower-collector/Scripts/player.gd
+++ tower-collector/Scripts/player.gd
@@ -3,6 +3,9 @@ extends Area2D
 @export var speed = 400
 var screen_size
 
+func attack() -> void:
+	print("attack")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -19,12 +22,19 @@ func _process(delta: float) -> void:
 		vel.x += 1
 	if Input.is_action_pressed("left"):
 		vel.x -= 1
+	if Input.is_action_just_pressed("attack"):
+		if vel.length() == 0:
+			attack()
 		
 	if vel.length() > 0:
 		vel = vel.normalized() * speed
-		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.play("playerWalk")
 	else:
-		$AnimatedSprite2D.stop()
+		if Input.is_action_just_pressed("attack"):
+			$AnimatedSprite2D.play("playerAttack")
+		else:
+			if not $AnimatedSprite2D.animation == "playerAttack":
+				$AnimatedSprite2D.stop()
 		
 	position += vel * delta
 	position = position.clamp(Vector2.ZERO, screen_size)	

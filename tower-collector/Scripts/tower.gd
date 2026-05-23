a4dcc1e51105844883e93c394d48887358482044
ayanp26 <ayanprasad26@gmail.com>
2026-05-23 13:27:25 -0400

    worked on collisions for tower

--- tower-collector/Scripts/tower.gd
+++ tower-collector/Scripts/tower.gd
@@ -1,11 +1,20 @@
-extends Area2D
+extends RigidBody2D
+
+signal hit
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	show()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deffered("disabled", true)
+	
---
+++ tower-collector/player.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+
+@export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var vel = Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		vel.y -= 1
+	if Input.is_action_pressed("down"):
+		vel.y += 1
+	if Input.is_action_pressed("right"):
+		vel.x += 1
+	if Input.is_action_pressed("left"):
+		vel.x -= 1
+		
+	if vel.length() > 0:
+		vel = vel.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += vel * delta
+	position = position.clamp(Vector2.ZERO, screen_size)	

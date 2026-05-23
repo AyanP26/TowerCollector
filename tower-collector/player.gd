4fbc839650da81340926123a9788f4153da34955
codeall1234 <1017061@lcps.org>
2026-05-23 11:40:23 -0400

    movement code

--- tower-collector/player.gd
+++ tower-collector/player.gd
@@ -8,4 +8,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var vel = Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		vel.y += 1
+	if Input.is_action_pressed("down"):
+		vel.y -= 1
+	if Input.is_action_pressed("right"):
+		vel.x += 1
+	if Input.is_action_pressed("left"):
+		vel.x -= 1
+		
+	if vel.length() > 0:
+		vel = vel.normalized() * 5
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+		

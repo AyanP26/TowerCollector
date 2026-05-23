ed1e8bd9c99b0d1e2511ae357bd2a1e7236eb47c
ayanp26 <ayanprasad26@gmail.com>
2026-05-23 14:24:46 -0400

    worked on enemy path

--- tower-collector/Scripts/level_1.gd
+++ tower-collector/Scripts/level_1.gd
@@ -1,6 +1,6 @@
 extends Node
 @export var enemy: PackedScene
-
+var score = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +9,30 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_enemy_timer_timeout() -> void:
+	var enemy = enemy.instantiate()
+	
+	var enemy_spawn_location = $EnemyPath/EnemySpawnLocation
+	
+	enemy.position = enemy_spawn_location.position
+	
+	var direction = enemy_spawn_location.rotation + PI/2
+	
+	direction += randf_range(-PI/4, PI/4)
+	enemy.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	enemy.linear_velocity = velocity.rotated(direction)
+	
+	add_child(enemy)	
+
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+
+func _on_start_timer_timeout() -> void:
+	$EnemyTimer.start()
+	$ScoreTimer.start()

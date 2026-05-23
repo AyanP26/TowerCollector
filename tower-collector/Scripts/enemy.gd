2afb6fd571cb7864ec6504b3f8903fd7aa5a8e5f
ayanp26 <ayanprasad26@gmail.com>
2026-05-23 18:44:32 -0400

    bg stuff

--- tower-collector/Scripts/enemy.gd
+++ tower-collector/Scripts/enemy.gd
@@ -10,8 +10,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position = 
-
+	pass
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()

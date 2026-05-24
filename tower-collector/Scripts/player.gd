2a0bcf840baba7f218354360c160513a66963e35
Ikshaan Kaul <amazingdude1940@outlook.com>
2026-05-23 20:14:07 -0400

    changed logic

--- tower-collector/Scripts/player.gd
+++ tower-collector/Scripts/player.gd
@@ -15,7 +15,6 @@ func build() -> void:
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var vel = Vector2.ZERO
